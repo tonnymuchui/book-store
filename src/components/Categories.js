cee1c00a6d6b25409732ff5d14ef54ff72dab58c
tonnymuchui <tonykanyingah@gmail.com>
2023-01-28 15:01:54 +0300

    Update categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,21 @@
-function Categories() {
-  return <button type="submit">Check Status</button>;
-}
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
+
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <>
+      <button type="submit" onClick={handleClick}>Check Status</button>
+      <p>{status}</p>
+    </>
+  );
+};
 
 export default Categories;
