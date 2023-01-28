43e0fec03109273baa1e4bd7e862aa181ce8c6c5
tonnymuchui <tonykanyingah@gmail.com>
2023-01-28 13:51:46 +0300

    Update components

--- src/components/Books.js
+++ src/components/Books.js
@@ -6,7 +6,7 @@ function Books() {
   return (
     <>
       <div className="books-shelf">
-        <Book title="My Book" author="Tonny" />
+        <Book title="My Book" author="Me" />
       </div>
       <AddNewBook />
     </>
