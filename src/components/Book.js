d19ef9b60b0fdce55f6b8d3ec534ebd0a394470e
tonnymuchui <tonykanyingah@gmail.com>
2023-01-28 15:00:53 +0300

    Update book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,31 @@
+import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-function Book(props) {
-  const { title, author } = props;
+const Book = (props) => {
+  const dispatch = useDispatch();
+
+  const { title, author, id } = props;
+
+  const handleClick = (id) => {
+    dispatch(removeBook(id));
+  };
+
   return (
-    <div className="books-shelf">
-      <h2>{title}</h2>
-      <span>{author}</span>
-      <button type="submit">Remove</button>
+    <div className="book-shelf">
+      <ul className="book-list">
+        <li>{title}</li>
+        <li>{author}</li>
+      </ul>
+      <button className="remove-btn" onClick={() => handleClick(id)} type="button">Remove</button>
     </div>
   );
-}
+};
 
 Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-};
+  title: PropTypes.string,
+  author: PropTypes.string,
+}.isRequired;
 
 export default Book;
