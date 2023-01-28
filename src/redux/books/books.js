31439533be7026d0e98b5b6a9cbc567a7e336559
tonnymuchui <tonykanyingah@gmail.com>
2023-01-28 13:53:28 +0300

    Add book reducer

---
+++ src/redux/books/books.js
@@ -0,0 +1,30 @@
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+
+const initState = {
+  books: [],
+};
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
+
+const booksReducer = (state = initState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
